99852e1fa7872a4b441bf65154d38f0f2c36dde8
TrisTaam <yadartcon@gmail.com>
2024-03-08 01:05:21 +0700

    feat: base code and fake data

---
+++ app/src/main/java/com/proptit/budgetbuddy/presentation/ui/transaction/TransactionsFragment.kt
@@ -0,0 +1,26 @@
+package com.proptit.budgetbuddy.presentation.ui.transaction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.proptit.budgetbuddy.databinding.FragmentTransactionsBinding
+
+class TransactionsFragment : Fragment() {
+    private var _binding: FragmentTransactionsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTransactionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
