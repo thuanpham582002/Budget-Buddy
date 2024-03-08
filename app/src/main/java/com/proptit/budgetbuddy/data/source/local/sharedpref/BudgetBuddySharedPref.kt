7abd85009c1bb7e4bf3a57a22acd1e61f76a187d
TrisTaam <yadartcon@gmail.com>
2024-03-08 17:21:56 +0700

    feat: base code and fake data

---
+++ app/src/main/java/com/proptit/budgetbuddy/data/source/local/sharedpref/BudgetBuddySharedPref.kt
@@ -0,0 +1,26 @@
+package com.proptit.budgetbuddy.data.source.local.sharedpref
+
+import android.app.Application
+import android.content.Context
+import javax.inject.Inject
+
+class BudgetBuddySharedPref @Inject constructor(
+    private val context: Application
+) {
+    companion object {
+        const val SHARED_PREF_NAME = "budget_buddy_shared_pref"
+    }
+
+    fun put(key: String, value: Int) {
+        val sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putInt(key, value)
+            apply()
+        }
+    }
+
+    fun get(key: String, defaultValue: Int): Int {
+        val sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getInt(key, defaultValue)
+    }
+}
